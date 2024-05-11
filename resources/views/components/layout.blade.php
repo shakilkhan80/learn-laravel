0525c3ebde3f899ac7f8c1db9373712e7ca0c780
shakilkhan80 <shakilkhan81198@gmail.com>
2024-05-11 23:31:00 +0600

    project started

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+</head>
+
+<body>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+    </nav>
+    {{ $slot }}
+</body>
+
+</html>
