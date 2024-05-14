6ec09b97ef49c074ec37c98f15234743904155d8
shakilkhan80 <shakilkhan81198@gmail.com>
2024-05-14 09:38:22 +0600

    dynamic route added

---
+++ resources/views/jobs.blade.php
@@ -0,0 +1,13 @@
+<x-layout>
+    <x-slot:heading>
+        Job Listing
+    </x-slot:heading>
+    <ul>
+        @foreach ($jobs as $job)
+            <li class="text-blue-500 hover:underline">
+                <a href="/jobs/{{ $job['id'] }}"><strong>{{ $job['title'] }}</strong>: pays {{ $job['salary'] }}
+                </a>
+            </li>
+        @endforeach
+    </ul>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,65 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        'greetings' => 'Hello World!',
+        'name'      => 'Laravel',
+    ]);
 });
-Route::get('/about', function () {
-    return view('about');
+
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => [
+            [
+                'id'      => 1,
+                'title'   => 'Developer',
+                'company' => 'Google',
+                'salary'  => 10000
+            ],
+            [
+                'id'      => 2,
+                'title'   => 'Marketer',
+                'company' => 'Youtube',
+                'salary'  => 30000
+            ],
+            [
+                'id'      => 3,
+                'title'   => 'Investor',
+                'company' => 'Tesla',
+                'salary'  => 200000
+            ],
+        ]
+    ]);
 });
+Route::get('/jobs/{id}', function ($id) {
+    $jobs = [
+        [
+            'id'      => 1,
+            'title'   => 'Developer',
+            'company' => 'Google',
+            'salary'  => 10000
+        ],
+        [
+            'id'      => 2,
+            'title'   => 'Marketer',
+            'company' => 'Youtube',
+            'salary'  => 30000
+        ],
+        [
+            'id'      => 3,
+            'title'   => 'Investor',
+            'company' => 'Tesla',
+            'salary'  => 200000
+        ],
+    ];
+
+    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+
+    return view('job', [
+        'job' => $job
+    ]);
+});
+
 Route::get('/contact', function () {
     return view('contact');
 });
