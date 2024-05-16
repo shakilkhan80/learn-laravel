53bdd8cbd4286b152af855d3b09a45d9a2dcb151
shakilkhan80 <shakilkhan81198@gmail.com>
2024-05-16 09:21:47 +0600

    forign key added

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Job extends Model
 {
+    use HasFactory;
     protected $table = 'job_listing';
 
-    protected $fillable = ['title', 'sallary']; 
+    protected $fillable = ['title', 'sallary'];
 
 }
