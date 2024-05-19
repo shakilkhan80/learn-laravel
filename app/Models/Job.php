eb8647bf85c34a19330f9609e93925bc1c44901f
shakilkhan80 <shakilkhan81198@gmail.com>
2024-05-19 23:31:02 +0600

    foreign key problem done

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Job extends Model
 {
     use HasFactory;
-    protected $table = 'job_listing';
+    protected $table = 'job_listings';
 
     protected $fillable = ['title', 'sallary'];
 
