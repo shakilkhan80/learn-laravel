41c306c1220b1bdba7a9b4bc474b187aa57bf64d
shakilkhan80 <shakilkhan81198@gmail.com>
2024-05-20 09:29:29 +0600

    lazy loading uses

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -2,12 +2,17 @@
     <x-slot:heading>
         Job Listing
     </x-slot:heading>
-    <ul>
+    <div class="spece-y-4">
         @foreach ($jobs as $job)
-            <li class="text-blue-500 hover:underline">
-                <a href="/jobs/{{ $job['id'] }}"><strong>{{ $job['title'] }}</strong>: pays {{ $job['sallary'] }}
-                </a>
-            </li>
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+                <div class="font-blod text-blue-400 text-sm">
+                    {{ $job->employer->name }}
+                </div>
+                <div>
+                    <strong>{{ $job['title'] }}</strong>: pays
+                    {{ $job['sallary'] }}
+                </div>
+            </a>
         @endforeach
-    </ul>
+    </div>
 </x-layout>
