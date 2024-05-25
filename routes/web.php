72812f1d28775e77db5eeabc95f4da5e36c6e71a
shakilkhan80 <shakilkhan81198@gmail.com>
2024-05-25 23:29:22 +0600

    route resource know

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,9 @@ use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
-Route::get('/jobs', [JobController::class, 'index']);
-Route::get('/jobs/create', [JobController::class, 'create']);
-Route::get('/jobs/{job}', [JobController::class, 'show']);
-Route::post('/jobs', [JobController::class, 'store']);
-Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-Route::patch('/jobs/{job}', [JobController::class, 'update']);
-Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
-
 Route::view('/contact', 'contact');
+
+Route::resource('jobs', JobController::class,[
+    'only' => ['show']
+]);
+
