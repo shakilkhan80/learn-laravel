a1febbb453b131d9529b1f01fe23f8c20490ea08
shakilkhan80 <shakilkhan81198@gmail.com>
2024-05-26 08:56:00 +0600

    route edit

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
-Route::resource('jobs', JobController::class,[
-    'only' => ['show']
-]);
+Route::resource('jobs', JobController::class);
 
