125f322136af43352202ccfb19905edf73839c49
shakilkhan80 <shakilkhan81198@gmail.com>
2024-05-28 23:11:28 +0600

    login and register page fix

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -21,7 +21,6 @@
                         </div>
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
-                                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                                 <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
                                 <x-nav-link href="/jobs" :active="request()->is('jobs')">Jobs</x-nav-link>
                                 <x-nav-link :active="request()->is('contact')" type="button">Contact</x-nav-link>
@@ -70,7 +69,6 @@
             <!-- Mobile menu, show/hide based on menu state. -->
             <div class="md:hidden" id="mobile-menu">
                 <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-                    <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                     <a href="/" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
                         aria-current="page">Home</a>
                     <a href="/about"
