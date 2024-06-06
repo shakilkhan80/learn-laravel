b0c67b215940c28ffa7fa8112fbede73a1ef9498
Shakil Khan <131842581+shakilkhan80@users.noreply.github.com>
2024-06-06 23:21:26 +0600

    Category added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
@@ -8,7 +9,8 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
-Route::resource('jobs', JobController::class);
+Route::resource('jobs', JobController::class); 
+Route::resource('category', CategoryController::class); 
 
 Route::get('/register', [RegisteredUserController::class, 'create']);
 Route::post('/register', [RegisteredUserController::class, 'store']);
